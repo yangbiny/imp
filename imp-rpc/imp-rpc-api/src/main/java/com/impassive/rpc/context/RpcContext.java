e83471a7094fec387a0a712355df18c1cf789984
impassive <tingfa.yang@duitang.com>
2021-12-14 14:26:59 +0800

    feature/impassive/add rpc context

--- imp-rpc/imp-rpc-api/src/main/java/com/impassive/rpc/context/RpcContext.java
+++ imp-rpc/imp-rpc-api/src/main/java/com/impassive/rpc/context/RpcContext.java
@@ -1,9 +1,36 @@
 package com.impassive.rpc.context;
 
+import com.impassive.rpc.invocation.Invocation;
+import com.impassive.rpc.invoker.Invoker;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Rpc上下文信息
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RpcContext {
 
+  private static final ThreadLocal<RpcContext> threadLocal = ThreadLocal.withInitial(
+      RpcContext::new);
 
+  private Invoker<?> invoker;
+
+  private Invocation invocation;
+
+  private Object[] params;
+
+  private Class[] paramsTypes;
+
+
+  public static RpcContext rpcContextInstance() {
+    return threadLocal.get();
+  }
+
+  public static void removeContext() {
+    threadLocal.remove();
+  }
 }
--- imp-rpc/imp-rpc-api/src/main/java/com/impassive/rpc/filter/ContextFilter.java
+++ imp-rpc/imp-rpc-api/src/main/java/com/impassive/rpc/filter/ContextFilter.java
@@ -1,5 +1,6 @@
 package com.impassive.rpc.filter;
 
+import com.impassive.rpc.context.RpcContext;
 import com.impassive.rpc.invocation.Invocation;
 import com.impassive.rpc.invoker.Invoker;
 import com.impassive.rpc.result.Result;
@@ -9,6 +10,15 @@ public class ContextFilter implements Filter {
 
   @Override
   public Result filter(Invoker<?> invoker, Invocation invocation) throws Throwable {
-    return invoker.invoke(invocation);
+    RpcContext rpcContext = RpcContext.rpcContextInstance();
+    rpcContext.setInvocation(invocation);
+    rpcContext.setInvoker(invoker);
+    rpcContext.setParamsTypes(invocation.getParamTypes());
+    rpcContext.setParams(invocation.getParams());
+    try {
+      return invoker.invoke(invocation);
+    }finally {
+      RpcContext.removeContext();
+    }
   }
 }
