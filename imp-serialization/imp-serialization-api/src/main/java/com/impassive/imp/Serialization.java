420f922e182b2a985b54d2328f0e9eac6d4db24b
impassive <tingfa.yang@duitang.com>
2022-02-22 19:14:17 +0800

    feature/impassive/add protobuf

--- imp-serialization/imp-serialization-api/src/main/java/com/impassive/imp/Serialization.java
+++ imp-serialization/imp-serialization-api/src/main/java/com/impassive/imp/Serialization.java
@@ -1,7 +1,6 @@
 package com.impassive.imp;
 
 import java.lang.reflect.Type;
-import java.util.List;
 import javax.annotation.Nullable;
 
 public interface Serialization {
@@ -29,6 +28,16 @@ public interface Serialization {
   @Nullable
   <T> T deserialization(byte[] bytes, Class<T> tClass);
 
+  /**
+   * 反序列化。将给定的字节数组，按照设定的 type的信息进行反序列化。
+   *
+   * <p>如果字节数组为空，则返回Null</p>
+   *
+   * @param bytes 需要进行反序列化的 直接数组
+   * @param type 反序列化的类型
+   * @param <T> 返回值类型
+   * @return 序列化结果。如果直接数组为空，则返回Null
+   */
   @Nullable
   <T> T deserialization(byte[] bytes, Type type);
 
